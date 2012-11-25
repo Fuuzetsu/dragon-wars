58720732aa161377dbb52874f6f2778f5945716a
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-25 22:54:44 +0000

    Add Player

---
+++ src/wars/dragon/engine/Player.java
@@ -0,0 +1,27 @@
+package wars.dragon.engine;
+
+import java.util.*;
+
+public class Player {
+    private String name;
+    private Boolean lost;
+    List<Unit> ownedUnits = new ArrayList<Unit>();
+
+    public class Player(String name) {
+	this.name = name;
+    }
+
+    public String getName() {
+	return this.name;
+    }
+
+    public Boolean hasLost() { 
+	return this.lost || ownedUnits.isEmpty();
+    }
+
+    public Boolean hasMoveableUnits() {
+	for (Unit u : ownedUnits)
+	    if (!u.hasFinishedTurn())
+		return true;
+	return false;
+    }
