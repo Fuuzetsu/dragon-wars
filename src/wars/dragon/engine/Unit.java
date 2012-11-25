5336e89599cf03ed4d27592216d3ba782221fac1
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-25 22:09:40 +0000

    Fix up Unit; add accessors

--- src/wars/dragon/engine/Unit.java
+++ src/wars/dragon/engine/Unit.java
@@ -2,31 +2,50 @@ package wars.dragon.engine;
 
 public abstract class Unit {
 
-    private Integer health;
+    private Integer maxMovement, movement;
+    private Double maxHealth, health;
     private Double attack, meleeDefense, rangeDefense;
     private Position position;
 
-    public Unit(Integer health, Double attack, 
+    public Unit(Integer maxHealth, Integer maxMovement, Double attack, 
 		Double meleeDefense, Double rangeDefense) {
-	this.health = health;
+	this.maxHealth = maxHealth;
+	this.health = this.maxHealth;
+
+	this.maxMovement = maxMovement;
+	this.movement = this.maxMovement;
+
 	this.attack = attack;
 	this.meleeDefense = meleeDefense;
 	this.rangeDefense = rangeDefense;
     }
 
     public Boolean isDead() {
-	return (health <= 0);
+	return health <= 0;
+    }
+
+    public Double getHealth() { return this.health; }
+    public Double getMaxHealth() {return this.maxHealth; }
+    public Double getAttack() { return this.attack; }
+    public Double getMeleeDefense() { return this.meleeDefense; }
+    public Double getRangeDefense() { return this.rangeDefense; }
+    public Position getPosition() { return this.position; }
+    public Integer getRemainingMovement() { return this.movement; }
+    public Integer getMaxMovement() { return this.maxMovement; }
+
+    public void reduceHealth(Double damage) {
+	this.health -= damage;
     }
 
     public void setPosition(Position position) {
 	this.position = position;
     }
 
-    public Integer getHealth() { return this.health; }
-    public Double getAttack() { return this.attack; }
-    public Double getMeleeDefense() { return this.meleeDefense; }
-    public Double getRangeDefense() { return this.rangeDefense; }
-    public Position getPosition() { return this.position; }
+    public void restoreHealth(Double heal) {
+	Double newHealth = this.health = heal;
+	this.health = (newHealth <= maxHealth) ? newHealth : maxHealth;
+    }
+    
 
 }
     
