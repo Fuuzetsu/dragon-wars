a59911bc8700d621ad7f7f840ae72c4e0c6332fb
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-26 02:57:17 +0000

    MapReader and example map test classes

---
+++ src/wars/dragon/engine/MapReader.java
@@ -0,0 +1,53 @@
+package wars.dragon.engine;
+/* Generates a GameField based on a flat text file. Test solution. */
+
+import java.util.*
+
+public class MapReader {
+    
+    public static GameField readMap(List<String> mapLines) {
+	int longestLine = -1;
+	for (String s : mapLines) {
+	    if (s.length() > longestLine)
+		longestLine = s.length();
+	}
+
+	List< List<Tile> > grid = 
+	    new ArrayList< List<Tile> >(mapLines.size());
+	
+	/* Initialize (height x width) grid */
+	for (Integer x = 0; x < mapLines.size(); x++) {
+	    List<Tile> inner = new ArrayList<Tile>(longestLine);
+	    grid.add(inner);
+	}
+
+	/* Fill out the grid with actual tiles */
+	for (Integer height = 0; height < mapLines.size(); height++) {
+	    for (Integer width = 0; width < longestLine; width++) {
+		if (mapLines.get(height).length() > width) {
+		    Character symbol = mapLines.get(height).charAt(width);
+		    grid.get(height).add(getTile(symbol));
+		}
+		else {
+		    grid.get(height).add(getTile(null));
+		}
+	    }
+	}
+	
+	return new GameField(grid);
+
+    }
+
+    private static Tile getTile(Character symbol) {
+	if (symbol == null || symbol == ' ')
+	    return new Pit();
+	switch(symbol) {
+	case 'G': return new Grass();
+	case 'W': return new Water();
+	default: 
+	    System.err.println("MapReader doesn't know the symbol " + symbol);
+	    System.exit(1);
+	    return null; /* Java, please. */
+	}
+    }	
+}
