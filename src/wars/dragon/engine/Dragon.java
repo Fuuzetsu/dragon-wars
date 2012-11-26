f2ca0f7168d3843743b71a04da5bda49cfceffaf
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-26 05:00:01 +0000

    More type trickery

--- src/wars/dragon/engine/Dragon.java
+++ src/wars/dragon/engine/Dragon.java
@@ -4,7 +4,7 @@ public class Dragon extends Unit implements RangedUnit {
     private String name = "Dragon";
 
     public Dragon() {
-	super(10, 5, 3, 3, 2);
+	super(10.0, 5, 3.0, 3.0, 2.0);
     }
 
     public Double getRange() {
