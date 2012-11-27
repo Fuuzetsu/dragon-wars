59d6d004b09cd97d199624b46c1b150100afba96
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2012-11-27 03:54:44 +0000

    Pre-test A* path finding

--- src/wars/dragon/engine/Logic.java
+++ src/wars/dragon/engine/Logic.java
@@ -6,6 +6,73 @@ import java.lang.*;
 /* Class containing things like damage calculation and path finding. */
 public class Logic {
     
+    public List<Position> findPath(Map map, Unit unit, Position destination) {
+	return AStar(map, unit, destination);
+    }
+
+    private List<Position> AStar(Map map, Unit unit, Position destination) {
+	Set<Position> expanded = new HashSet<Position>();
+	Comparator< Pair<List<Position>, Double> > comp = new AStarComparator();
+	PriorityQueue< Pair<List<Position>, Double> > queue = 
+	    new PriorityQueue< Pair<List<Position>, Double> >(10, comp);
+	List<Position> root = new ArrayList<Position>();
+	root.add(unit.getPosition());
+	queue.add(new Pair<List<Position>, Double>(root, 0.0));
+	while (queue.size() != 0) {
+	    Pair<List<Position>, Double> posP = queue.poll();
+	    List<Position> poss = posP.getLeft();
+	    Position lastPos = poss.get(poss.size() - 1);
+	    if (lastPos.equals(destination))
+		return poss;
+
+	    if (expanded.contains(lastPos))
+		continue;
+
+	    expanded.add(lastPos);
+
+	    /* Get heuristic */
+	    Integer h = getManhattanDistance(lastPos, destination);
+	    /* Get cost */
+	    Double g = getMovementCost(map, unit, lastPos);
+	    Double pathCost = h + g + posP.getRight();
+	    for (Position p : getAdjacentPositions(lastPos)) {
+		if (map.isValidField(p)) {
+		    List<Position> plan = new ArrayList<Position>(poss);
+		    plan.add(p);
+		    queue.add(new Pair<List<Position>, Double>(plan, pathCost));
+		}
+	    }		
+	}
+	List<Position> dummy = new ArrayList<Position>();
+	return dummy; /* Search failed */
+    }
+
+    private List<Position> getAdjacentPositions(Position pos) {
+	List<Position> positions = new ArrayList<Position>();
+	positions.add(new Position(pos.getX(), pos.getY() + 1));
+	positions.add(new Position(pos.getX(), pos.getY() - 1));
+	positions.add(new Position(pos.getX() + 1, pos.getY()));
+	positions.add(new Position(pos.getX() - 1, pos.getY()));
+	return positions;
+    }
+
+    private class AStarComparator implements Comparator< Pair<List<Position>, Double> > {
+	public int compare(Pair<List<Position>, Double> p1, Pair<List<Position>, Double> p2) {
+	    if (p1.getRight() < p2.getRight())
+		return -1;
+	    if (p1.getRight() > p2.getRight())
+		return 1;
+
+	    return 0;
+	}
+    }
+
+    private Double getMovementCost(Map map, Unit unit, Position origin) {
+	/* g(x) for search */
+	// flying units ignore this; always 1
+	return (100 / map.getField(origin).getMovementModifier());
+    }
+    
     public Set<Position> getAttackableUnits(Map map, Unit unit) {
 	Set<Position> atkFields = getAttackableFields(map, unit);
 	Set<Position> atkUnits = new HashSet<Position>();
@@ -18,14 +85,14 @@ public class Logic {
 
     private Set<Position> getAttackableFields(Map map, Unit unit) {
 	if (!unit.isRanged())
-	    return getPositionsInRange(unit.getPosition(), 1.0);
+	    return getPositionsInRange(map, unit.getPosition(), 1.0);
 
 	RangedUnit ru = (RangedUnit) unit;
-	return getPositionsInRange(ru.getPosition(), 
+	return getPositionsInRange(map, ru.getPosition(), 
 				   ru.getMinRange(), ru.getMaxRange());
     }
 
-    private Set<Position> getPositionsInRange(Position origin, Double range) {
+    private Set<Position> getPositionsInRange(Map map, Position origin, Double range) {
 	Set<Position> positions = new HashSet<Position>();
 	Double maxr = Math.ceil(range);
 	
@@ -44,9 +111,9 @@ public class Logic {
 		else if (y > maxr)
 		    newP = new Position(newP.getX(), newP.getY() + y);
 
-		if (newP.equals(origin))
+		if (newP.equals(origin) || !map.isValidField(newP))
 		    continue;
-
+		
 		positions.add(newP);
 		
 	    }
@@ -54,9 +121,9 @@ public class Logic {
 	return positions;		
     }
 
-    private Set<Position> getPositionsInRange(Position origin, 
+    private Set<Position> getPositionsInRange(Map map, Position origin, 
 					      Double minRange, Double maxRange) {
-	Set<Position> positions = getPositionsInRange(origin, maxRange);
+	Set<Position> positions = getPositionsInRange(map, origin, maxRange);
 	Set<Position> filtered = new HashSet<Position>();
 	for (Position p : positions) {
 	    Pair<Integer, Integer> dist;
@@ -73,6 +140,7 @@ public class Logic {
     /* Used as a heuristic for A* */
     private Integer getManhattanDistance(Position origin, 
 					 Position destination) {
+	/* h(x) */
 	Pair<Integer, Integer> distance = getDistanceAway(origin, 
 							  destination);
 
