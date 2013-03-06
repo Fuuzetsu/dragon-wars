440b4a764a526b858f42581eac7d76f3eee8fa7a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-06 15:28:34 +0000

    basic JSON loading

--- src/com/group7/dragonwars/engine/MapReader.java
+++ src/com/group7/dragonwars/engine/MapReader.java
@@ -3,16 +3,41 @@ package com.group7.dragonwars.engine;
 /* Generates a GameField based on a flat text file. Test solution. */
 
 import java.util.*;
+import org.json.*;
 
 public class MapReader {
 
-    public static Map readMap(List<String> mapLines) {
+    public static Map readMap(List<String> mapLines) throws JSONException {
         int longestLine = -1;
+		String jsonSource = "";
 
         for (String s : mapLines) {
             if (s.length() > longestLine)
                 longestLine = s.length();
+			jsonSource += s + "\n";
         }
+
+		System.out.println("Printing raw JSON map.");
+		System.out.println(jsonSource);
+
+
+		JSONObject m = new JSONObject(jsonSource);
+		String mapName = m.getString("mapName");
+		Integer sizeX = m.getInt("sizeX");
+		Integer sizeY = m.getInt("sizeY");
+		Integer players = m.getInt("players");
+
+		HashMap<Character, GameField> fields = new HashMap<Character, GameField>();
+
+		JSONObject fs = m.getJSONObject("fields");
+		Iterator<?> iter = fs.keys();
+		while (iter.hasNext()) {
+			String key = (String) iter.next(); /* We have to cast ;_; */
+			fields.put(key.charAt(0), MapReader.getGameField(key.charAt(0))); /* TODO GF handling */
+		}
+
+
+
 
         List<List<GameField>> grid = new ArrayList<List<GameField>>(longestLine);
 
@@ -27,7 +52,7 @@ public class MapReader {
             for (Integer width = 0; width < longestLine; width++) {
                 if (mapLines.get(height).length() > width) {
                     Character symbol = mapLines.get(height).charAt(width);
-                    grid.get(height).add(getGameField(symbol));
+                    grid.get(height).add(fields.get(symbol));
                 } else {
                     grid.get(height).add(getGameField(null));
                 }
