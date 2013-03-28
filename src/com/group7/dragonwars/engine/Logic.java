14f58cc4a38a44b582478f050a2371c8e4edd799
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-28 14:33:52 +0000

    remove no longer relevant comment

--- src/com/group7/dragonwars/engine/Logic.java
+++ src/com/group7/dragonwars/engine/Logic.java
@@ -104,8 +104,6 @@ public class Logic {
     }
 
     private List<Position> AStar(GameMap map, Unit unit, Position destination) {
-    	// setting path to true will cause the original behaviour, the path between unit and destination will be returned
-    	// otherwise destination is ignored and the list of possible destinations for unit (based on the unit's maxMovement) is instead returned
         if (!map.isValidField(destination))
             return new ArrayList<Position>(0);
 
