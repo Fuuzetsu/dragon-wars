21c55bba363d2b6a122ec63afa60ba9ebd944c15
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-30 23:44:53 +0000

    remove now unused import

--- src/com/group7/dragonwars/GameActivity.java
+++ src/com/group7/dragonwars/GameActivity.java
@@ -45,7 +45,6 @@ import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
