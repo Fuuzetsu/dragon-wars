323ca2c458a671e61b16d49dd781a454888c3a0a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-10 00:32:06 +0100

    Naively split up GameActivity

---
+++ src/com/group7/dragonwars/engine/FloatPair.java
@@ -0,0 +1,38 @@
+package com.group7.dragonwars.engine;
+
+public class FloatPair {
+    private Pair<Float, Float> pair;
+
+    public FloatPair(final Float x, final Float y) {
+        this.pair = new Pair<Float, Float>(x, y);
+    }
+
+    public Float getX() {
+        return this.pair.getLeft();
+    }
+
+    public Float getY() {
+        return this.pair.getRight();
+    }
+
+
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof FloatPair)) {
+            return false;
+        }
+
+        FloatPair that = (FloatPair) other;
+        return this.getX() == that.getX() && this.getY() == that.getY();
+
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", this.pair.getLeft(),
+                             this.pair.getRight());
+    }
+}
