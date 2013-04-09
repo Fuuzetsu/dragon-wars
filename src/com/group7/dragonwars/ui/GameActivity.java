241713ce8e5b2ea7aeb0e76f42b1668e94c0808a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-10 00:16:13 +0100

    Put the UI files in their own (sub)-package

--- src/com/group7/dragonwars/ui/GameActivity.java
+++ src/com/group7/dragonwars/ui/GameActivity.java
@@ -1,4 +1,4 @@
-package com.group7.dragonwars;
+package com.group7.dragonwars.ui;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
--- src/com/group7/dragonwars/ui/MainMenuActivity.java
+++ src/com/group7/dragonwars/ui/MainMenuActivity.java
@@ -1,4 +1,4 @@
-package com.group7.dragonwars;
+package com.group7.dragonwars.ui;
 
 import android.os.Bundle;
 import android.app.Activity;
